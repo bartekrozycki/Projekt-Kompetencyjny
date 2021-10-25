00627208e58a0b77ea021d5a01bf20d779d7a4b8
Tomasz Sztajkowski <46276902+tomaszsztajkowski@users.noreply.github.com>
2021-10-25 21:48:49 +0200

    v0.0.1

--- run.py
+++ run.py
@@ -1,3 +1,5 @@
+import time
+
 import pygame
 from pygame.locals import *
 
@@ -9,16 +11,65 @@ class App:
         """Initialize pygame and the application."""
         pygame.init()
         flags = RESIZABLE
-        App.screen = pygame.display.set_mode((640, 640), flags)
+        self.screen = pygame.display.set_mode((640, 640), flags)
+        self.pos_x = 0
+        self.pos_y = 0
+        self.density = 10
+        self.zoom = 1
+        self.moving = False
+        self.prev_mouse_pos = None
+        self.test = [(0, 0)]
 
-        App.running = True
+        self.running = True
+
+    def draw_grid(self):
+        win_width, win_height = self.screen.get_size()
+
+
+        x = self.pos_x % self.density
+        y = self.pos_y % self.density
+        while x < win_width:
+            while y < win_height:
+                pygame.draw.rect(self.screen, (0, 0, 0), (x, y, 1, 1), 1)
+                y += self.density
+            y = self.pos_y % self.density
+            x += self.density
+
+    def render(self):
+        self.screen.fill((255, 255, 255))  # White background
+        self.draw_grid()
+        for pos in self.test:
+            pygame.draw.circle(self.screen, (255, 0, 0), (self.pos_x + pos[0] + 1, self.pos_y + pos[1] + 1), 4 * self.zoom, 1)
+        pygame.display.update()
 
     def run(self):
         """Run the main event loop."""
-        while App.running:
+        while self.running:
             for event in pygame.event.get():
                 if event.type == QUIT:
-                    App.running = False
+                    self.running = False
+                elif event.type == pygame.MOUSEBUTTONDOWN:
+                    if event.button == pygame.BUTTON_MIDDLE:
+                        self.moving = True
+                    elif event.button == pygame.BUTTON_WHEELUP:
+                        self.zoom *= 1.125
+                        self.density *= 1.125
+                    elif event.button == pygame.BUTTON_WHEELDOWN:
+                        self.zoom /= 1.125
+                        self.density /= 1.125
+                elif event.type == pygame.MOUSEBUTTONUP:
+                    if event.button == pygame.BUTTON_MIDDLE:
+                        self.moving = False
+                        self.prev_mouse_pos = None
+                elif event.type == pygame.MOUSEMOTION and self.moving:
+                    try:
+                        mouse_pos = pygame.mouse.get_pos()
+                        self.pos_x += mouse_pos[0] - self.prev_mouse_pos[0]
+                        self.pos_y += mouse_pos[1] - self.prev_mouse_pos[1]
+                    except:
+                        pass
+                    self.prev_mouse_pos = pygame.mouse.get_pos()
+            self.render()
         pygame.quit()
 
 
