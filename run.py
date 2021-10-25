0a97684af84be45043d1f54e440b846ffcfffb6b
Różycki, Bartosz <bartek.rozycki@icloud.com>
2021-10-25 20:16:50 +0200

    Create project with python and pygame. Closes #4

---
+++ run.py
@@ -0,0 +1,26 @@
+import pygame
+from pygame.locals import *
+
+
+class App:
+    """Create a single-window app with multiple scenes."""
+
+    def __init__(self):
+        """Initialize pygame and the application."""
+        pygame.init()
+        flags = RESIZABLE
+        App.screen = pygame.display.set_mode((640, 640), flags)
+
+        App.running = True
+
+    def run(self):
+        """Run the main event loop."""
+        while App.running:
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                    App.running = False
+        pygame.quit()
+
+
+if __name__ == '__main__':
+    App().run()
